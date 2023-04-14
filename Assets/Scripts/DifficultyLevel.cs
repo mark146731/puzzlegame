e4ae468556ec10cb1c592e0262e55054a92a0214
mark146731 <mark.146731@nv.ccsd.net>
2023-04-14 10:16:07 -0700

    fixed a bunch of things

---
+++ Assets/Scripts/DifficultyLevel.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DifficultyLevel : MonoBehaviour
+{
+    public static string difficulty;
+
+    public void Start()
+    {
+
+    }
+    public void Update()
+    {
+
+    }
+    
+    
+    public void setDifficultyLevelEasy()
+    {
+        difficulty = "Easy";
+    }
+    public void setDifficultyLevelNormal()
+    {
+        difficulty = "Normal";
+
+    }
+    public void setDifficultyLevelHard()
+    {
+        difficulty = "Hard";
+    }
+    public static string getDifficultLevel()
+    {
+        return difficulty;
+    }
+}
