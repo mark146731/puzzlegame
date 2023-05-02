2fcf4abb7da30a87af615615f124a8eb617e5916
mark146731 <mark.146731@nv.ccsd.net>
2023-05-02 09:04:40 -0700

    Revert "temp"
    This reverts commit 4ecd111cbbdf6b92624482af33a2b262569ae16b.

--- Assets/Scripts/DeathScreen.cs
+++ Assets/Scripts/DeathScreen.cs
@@ -3,15 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class DeathScreen : MonoBehaviour
 {
+    int temp;
     public void Start()
     {
-
+        temp = SceneManager.GetActiveScene().buildIndex;
     }
 
     public void RestartGame ()
     {
-        int curr = CaveManBuildIndex.getBuildIndex();
-        SceneManager.LoadScene(curr);
+        SceneManager.LoadScene(temp);
     }
 
   public void ReturnMenu ()
