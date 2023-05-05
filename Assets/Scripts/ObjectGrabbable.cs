d0852c201e8319ac581447c885634c5dfb13a20e
mark146731 <mark.146731@nv.ccsd.net>
2023-05-05 09:34:18 -0700

    test

--- Assets/Scripts/ObjectGrabbable.cs
+++ Assets/Scripts/ObjectGrabbable.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class ObjectGrabbable : MonoBehaviour
 {
     private Rigidbody objectRigidbody;
     private Transform objectGrabPointTransform;
+    public GameObject text;
+
+    public GameObject temp1;
+    public GameObject temp2;
+    public GameObject temp3;
+
+
     private void Awake()
     {
         objectRigidbody = GetComponent<Rigidbody>();        
@@ -13,12 +22,33 @@ public class ObjectGrabbable : MonoBehaviour
     public void Grab(Transform objectGrabPointTransform)
     {
         this.objectGrabPointTransform = objectGrabPointTransform;
-        objectRigidbody.useGravity = false;
+        string tag = temp1.gameObject.tag;
+        if (tag == "CrateKey")
+        {
+            tag = "[Red]";
+        }
+        string tag1 = temp2.gameObject.tag;
+        {
+            if (tag1 == "LampKey")
+            {
+                tag = "[White]";
+
+            }
+            string tag3 = temp3.gameObject.tag;
+            if (tag3 == "RockKey")
+            {
+                tag = "[Green]";
+            }
+            text.GetComponent<TMP_Text>().text = tag;
+            text.SetActive(true);
+            objectRigidbody.useGravity = false;
+        }
     }
     public void Drop()
     {
         this.objectGrabPointTransform = null;
         objectRigidbody.useGravity = true;
+        text.SetActive(false);
     }
     private void FixedUpdate()
     {
@@ -28,5 +58,9 @@ public class ObjectGrabbable : MonoBehaviour
             Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabPointTransform.position, Time.deltaTime * lerpSpeed);
             objectRigidbody.MovePosition(newPosition);
         }
+    }
+    public void Start()
+    {
+        text.SetActive(false);
     }
 }
